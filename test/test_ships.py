50bdfffe6837ec7755507e61212478518fa357ff
mayragruiz <mayrag.ruiz11@gmail.com>
2023-08-15 21:04:16 -0300

    ship2

---
+++ test/test_ships.py
@@ -0,0 +1,12 @@
+import pytest
+import sys
+sys.path.append(r".\G11_PuellesRuiz_FinalBoss")
+from src.ship import Ship
+
+def test_ship1(): 
+    barco1=Ship(50,1)
+    assert barco1.is_worth_it()>20 == True
+def test_ship2():
+    barco2=Ship(20,1)
+    with pytest.raises(Exception):
+        barco2.is_worth_it()
